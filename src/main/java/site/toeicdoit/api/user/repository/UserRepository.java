5a21d39b21539580c9df1980fef9d96c0ded8946
seunghochoi9 <jackpot97912@gmail.com>
2025-03-28 18:43:43 +0900

    first commit

---
+++ src/main/java/site/toeicdoit/api/user/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package site.toeicdoit.api.user.repository;
+
+import site.toeicdoit.api.user.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel,Long> {
+    Optional<UserModel> findUserByUsername(String username);
+    List<UserModel> findUsersByName(String name);
+    List<UserModel> findUsersByJob(String job);
+
+    @Modifying
+    @Query("update users set accessToken =:accessToken where id =:id" )
+    void modifyTokenByToken(@Param("id") Long id, @Param("accessToken") String accessToken);
+
+    @Query("select a from users a where a.username =:username" )
+    UserModel existsByUsername(@Param("username") String username);
+
+    @Modifying
+    @Query("UPDATE users u SET u.password = :password, u.job = :job, u.addressId = :addressId WHERE u.id = :id")
+    void updateUserById(@Param("id") Long id, @Param("password") String password, @Param("job") String job, @Param("addressId") String addressId);
+
+    List<UserModel> findAllByOrderByIdDesc();
+}
